7def62735202518e3b7eafb087422071173dd18e
Swoad-11 <toufiq11swoad@gmail.com>
2025-09-15 20:00:12 +0600

    workling with singlePage

--- functions.php
+++ functions.php
@@ -2,10 +2,21 @@
 
 function demoTheme_theme_support() {
     add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 add_action("after_setup_theme","demoTheme_theme_support");
 
+function demoTheme_menus(){
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+    );
 
+    register_nav_menus($locations);
+}
+
+add_action('init', 'demoTheme_menus');
 
 function demoTheme_register_styles() {
 // FontAwesome
--- header.php
+++ header.php
@@ -6,16 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="/wp-content/themes/demoTheme/assets/images/logo.png"> 
 
     <?php wp_head(); ?>
 
 </head> 
 
 <body>
-    
     <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0 mt-4" href="index.html">SiteName.dev</a>
+	    <a class="site-title pt-lg-4 mb-0 mt-4" href="index.html">
+            <?php echo bloginfo('name'); ?>
+        </a>
         
 	   <nav class="navbar navbar-expand-lg navbar-dark">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" 
@@ -24,28 +25,26 @@
     </button>
 
     <div id="navigation" class="collapse navbar-collapse flex-column">
-        <img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo">			
 
-        <ul class="navbar-nav flex-column text-sm-center text-md-left">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-home fa-fw me-2"></i>Blog Home 
-                    <span class="visually-hidden">(current)</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="post.html"><i class="fas fa-file-alt fa-fw me-2"></i>Blog Post</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="page.html"><i class="fas fa-file-image fa-fw me-2"></i>Blog Page</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="archive.html"><i class="fas fa-archive fa-fw me-2"></i>Blog Archive</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw me-2"></i>Contact Us</a>
-            </li>
-        </ul>
+    <?php
+        if (function_exists('the_custom_logo')) {
+            $custom_logo_id = get_theme_mod('custom_logo');
+            $logo = wp_get_attachment_image_src($custom_logo_id);
+        }
+        ?>
+        <img class="mb-3 mx-auto logo" src=<?= $logo[0] ?> alt="logo">			
+
+		<?php
+		wp_nav_menu(
+			array(
+				'menu' => 'primary',
+				'container' => '',
+				'theme_location' => 'primary',
+				'items_wrap' => '<ul id="" class="navbar-nav flex-column text-sm-center text-md-left">%3$s</ul>',
+			)
+		);
+		?>
+
         <hr>
         <ul class="social-list list-inline py-3 mx-auto">
             <li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
